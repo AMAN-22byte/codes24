f8d87d42403a39f3ee9196c2601dc37943b2c80b
AMAN-22byte <gettoknowaman@gmail.com>
2025-06-16 00:00:19 +0530

    client

--- client/src/components/Codearena/Codearena.jsx
+++ client/src/components/Codearena/Codearena.jsx
@@ -106,7 +106,8 @@ function Codearena({ problemTitle }) {
   const handleAiReview = async () => {
     try {
       setLoadingAi(true);
-      const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/ai-review`, { code });
+      // const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/ai-review`, { code });
+      const { data } = await axios.post(`${import.meta.env.VITE_GOOGLE_GEMINI_API_URL}/ai-review`, { code });
       setAiReview(data.review);
     } catch (error) {
       setAiReview('Error in AI review, error: ' + error.message);
