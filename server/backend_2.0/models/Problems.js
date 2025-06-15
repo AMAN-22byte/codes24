b776821f6acd32e9c4cd5fc8dc57ff7eeee4b0f5
AMAN-22byte <gettoknowaman@gmail.com>
2025-06-15 20:46:36 +0530

    combine_server

---
+++ server/backend_2.0/models/Problems.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const problemSchema = new mongoose.Schema({
+  Title: {
+    type: String,
+    required: true,
+  },
+  Description: {
+    type: String,
+    required: true,
+  },
+  Difficulty: {
+    type: String,
+    required: true,
+  },
+  Tags: {
+    type: [String],
+    required: true,
+  },
+  Testcase: {
+    type: [String],
+    required: true,
+  },
+  hiddenTC:[
+     {
+      input: { type: String, required: true },
+      output: { type: String, required: true },
+    },
+  ],
+  createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    }
+});
+
+module.exports=mongoose.model("Problem", problemSchema)
