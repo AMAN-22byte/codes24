cfb66676644d7dc4e7ce2e37981c1fa25094c1e2
AMAN-22byte <gettoknowaman@gmail.com>
2025-06-15 15:18:28 +0530

    client_2.1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import SignUp from "../Components/SignUp"; // this includes login modal
+import SignUp from "../components/SignUp"; // this includes login modal
 import { useAuth } from "../AuthContext"; // ✅ import AuthContext
 
 const Navbar = () => {
